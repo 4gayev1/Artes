7e4556509b26e76f4c03f711f30041ce4983c8b4
4gayev1 <vahid.aghayev@ailab.az>
2025-02-03 12:14:22 +0400

    addCookies can handle POM

--- src/helper/stepFunctions/browserActions.js
+++ src/helper/stepFunctions/browserActions.js
@@ -1,11 +1,21 @@
-const { context } = require("../imports/commons");
+const { context, element } = require("../imports/commons");
 
 const browser = {
   setCookies: async (cookies) => {
-    await context.browser.addCookies([JSON.parse(cookies)]);
+    let cookieData;
+
+  try {
+    cookieData = JSON.parse(cookies);
+  } catch {
+    cookieData = element(cookies);
+  }
+
+  cookieData = Array.isArray(cookieData) ? cookieData : [cookieData];
+
+  await context.browser.addCookies(cookieData);
   },
 };
 
 module.exports = {
-  browser,
+  browser
 };
