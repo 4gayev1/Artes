d04fbd8990fbee7375ec2d94601a0c776a00a792
4gayev1 <vahid.aghayev@ailab.az>
2025-06-17 16:26:16 +0400

    Updates version and improves response handling logic
    - Bumps the package version from 1.0.76 to 1.0.77.
    - Enhances logic for processing API response arrays by adding conditional handling for empty arrays.
    - Imports `api` helper for improved code modularity.

--- src/stepDefinitions/random.steps.js
+++ src/stepDefinitions/random.steps.js
@@ -1,4 +1,5 @@
 const { Given, context, random } = require("../helper/imports/commons");
+const { api } = require("../helper/stepFunctions/exporter");
 
 Given("User sets random words as {string} variable", async (key) => {
   const words = random.lorem.words({ min: 2, max: 5 });
@@ -17,7 +18,12 @@ Given(
   "User sends GET request to {string} and save {string} variable from {string} array as a {string} randomly",
   async (endPoint, varName, fromArray, variableKey) => {
     await api.get(endPoint);
-    const responseBody = await context.response["Response Body"][fromArray == [] ? 0 : fromArray]
+    let responseBody
+    if(fromArray=="[]"){
+       responseBody = await context.response["Response Body"]
+    }else{
+       responseBody = await context.response["Response Body"][fromArray]
+    }
     const randomContent =
     responseBody[random.number.int({ min: 0, max: responseBody.length - 1 })];
     context.vars[variableKey] = randomContent[varName];
