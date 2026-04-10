87bd14272fad39429298f1d0fcbb0bd2b22fd7b1
4gayev1 <aghayevvahil1@gmail.com>
2026-04-11 02:51:24 +0400

    - hotfix / Added context object to context.js - Bumps version 1.7.6 -> 1.7.7

--- src/hooks/context.js
+++ src/hooks/context.js
@@ -4,6 +4,9 @@ class Context {
     this.page = undefined;
     this.request = undefined;
     this.response = undefined;
+    this.pickle = undefined;
+    this.step = undefined;
+    this.pickleResult = undefined;
     this.vars = undefined;
   }
 }
