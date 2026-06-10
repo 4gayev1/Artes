b9781acb236f0e6411bc17785564549707aa1938
4gayev1 <aghayevvahid1@gmail.com>
2026-06-11 02:40:57 +0400

    - Removed unnecessary imports

--- src/stepDefinitions/report.steps.js
+++ src/stepDefinitions/report.steps.js
@@ -1,6 +1,4 @@
 const { Given } = require("../helper/imports/commons");
-require("allure-cucumberjs");
-const allure = require("allure-js-commons");
 
 Given('{string}', (s) => {
 // This step is for adding description between the steps to the report
