35d64375414c09216a1ee612a26d223aba1251e7
4gayev1 <aghayevvahid1@gmail.com>
2026-06-11 02:21:19 +0400

    - Added step for description between steps

---
+++ src/stepDefinitions/report.steps.js
@@ -0,0 +1,7 @@
+const { Given } = require("../helper/imports/commons");
+require("allure-cucumberjs");
+const allure = require("allure-js-commons");
+
+Given('{string}', (s) => {
+// This step is for adding description between the steps to the report
+})
